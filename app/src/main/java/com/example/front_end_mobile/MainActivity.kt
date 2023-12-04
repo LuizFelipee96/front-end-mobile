2f7c3a608c43cd2e46b79f839e9fe7877e9280cf
Luiz Felipe <52084917+LuizFelipee96@users.noreply.github.com>
2023-12-04 13:59:23 -0300

    att main

--- app/src/main/java/com/example/front_end_mobile/MainActivity.kt
+++ app/src/main/java/com/example/front_end_mobile/MainActivity.kt
@@ -2,45 +2,44 @@ package com.example.front_end_mobile
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.front_end_mobile.ui.theme.FrontendmobileTheme
+import com.google.firebase.auth.FirebaseAuth
+import android.widget.EditText
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            FrontendmobileTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+        setContentView(R.layout.activity_login)
+
+        auth = FirebaseAuth.getInstance()
+
+        val usernameEditText = findViewById<EditText>(R.id.username)
+        val passwordEditText = findViewById<EditText>(R.id.password)
+        val loginButton = findViewById<Button>(R.id.login_button)
+
+        loginButton.setOnClickListener {
+            val email = usernameEditText.text.toString()
+            val password = passwordEditText.text.toString()
+
+            login(email, password)
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FrontendmobileTheme {
-        Greeting("Android")
+    private fun login(email: String, password: String) {
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Login bem-sucedido, atualizar a UI com as informações do usuário
+                    val user = auth.currentUser
+                    // Navegue para a próxima tela ou atualize a UI
+                } else {
+                    Toast.makeText(baseContext, "Autenticação falhou.",
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
