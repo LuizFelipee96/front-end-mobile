8275cd53fb20fdbd8cae0c4089f7823a4b42bc85
Luiz Felipe <52084917+LuizFelipee96@users.noreply.github.com>
2023-12-06 21:20:58 -0300

    atividade de add_treino adicionada

---
+++ app/src/main/java/com/example/front_end_mobile/AddTreinoActivity.kt
@@ -0,0 +1,11 @@
+package com.example.front_end_mobile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddTreinoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.add_treino)
+    }
+}
