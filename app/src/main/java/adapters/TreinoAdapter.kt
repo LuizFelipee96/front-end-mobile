574a96766892a1c02939293e74cd27bcb5b0d5ca
Luiz Felipe <52084917+LuizFelipee96@users.noreply.github.com>
2023-12-11 15:10:24 -0300

    att info

---
+++ app/src/main/java/adapters/TreinoAdapter.kt
@@ -0,0 +1,33 @@
+package adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.front_end_mobile.Treino
+
+class TreinoAdapter(private var treinos: List<Treino>) : RecyclerView.Adapter<TreinoAdapter.TreinoViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TreinoViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_treino, parent, false)
+        return TreinoViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: TreinoViewHolder, position: Int) {
+        holder.bind(treinos[position])
+    }
+
+    override fun getItemCount(): Int = treinos.size
+
+    fun updateTreinos(newTreinos: List<Treino>) {
+        treinos = newTreinos
+        notifyDataSetChanged()
+    }
+
+    class TreinoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        fun bind(treino: Treino) {
+            // data to UI
+        }
+    }
+}
