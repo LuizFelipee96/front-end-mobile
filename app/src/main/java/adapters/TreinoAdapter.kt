f5d0516bee80d2c36dcdf277f64d957013f73937
joaoveeiga <joao.moraes@clin.digital>
2023-12-12 11:49:47 -0300

    (fix): crash after navigating

--- app/src/main/java/adapters/TreinoAdapter.kt
+++ app/src/main/java/adapters/TreinoAdapter.kt
@@ -6,11 +6,12 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.front_end_mobile.Treino
+import com.example.front_end_mobile.R
 
 class TreinoAdapter(private var treinos: List<Treino>) : RecyclerView.Adapter<TreinoAdapter.TreinoViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TreinoViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_treino, parent, false)
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.add_treino, parent, false)
         return TreinoViewHolder(view)
     }
 
